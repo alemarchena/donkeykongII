cf2cfd9527fff77c0c5a67b04e651b8416b6fe55
Alejandro Marchena <alemarchena@gmail.com>
2023-03-13 12:15:16 -0300

    Primer push

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    Transform t;
+    Movement m;
+
+    private void Awake()
+    {
+        t = GetComponent<Transform>();
+        m = GetComponent<Movement>();
+    }
+
+    public void Up()
+    {
+        m.Move(t,Movement.TipoMovimiento.tup);
+    }
+
+    public void Down()
+    {
+        m.Move(t, Movement.TipoMovimiento.tdown);
+    }
+
+    public void Left()
+    {
+        m.Move(t, Movement.TipoMovimiento.tleft);
+    }
+
+    public void Right()
+    {
+        m.Move(t, Movement.TipoMovimiento.tright);
+    }
+
+    public void Jump()
+    {
+        m.Move(t, Movement.TipoMovimiento.tpush);
+    }
+}
